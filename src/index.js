66caae7ed77caa8c52177dc08be998e37e73b525
milen-ale <milenalebel2@gmail.com>
2022-08-03 04:31:28 -0700

    setup webpack

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './index.css';
+const tabel = document.getElementById('tabel');
+tabel.innerHTML = `  <tr>
+<td>Name:70</td>
+</tr>
+<tr>
+<td>Name:70</td>
+</tr>
+<tr>
+<td>Name:70</td>
+</tr>
+<tr>
+<td>Name:70</td>
+</tr>
+<tr>
+<td>Name:70</td>
+</tr><tr>
+<td>Name:70</td>
+</tr>
+<tr>
+<td>Name:70</td>
+</tr>`;
